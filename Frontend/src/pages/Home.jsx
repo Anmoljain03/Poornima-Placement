5d7755de767d1afedd1b9d4847eed180cbe66460
Anmol Jain <honeyjain245@gmail.com>
2025-02-18 20:26:39 +0530

    by aj

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
   
   return (
     <div>
-      <section className="text-center space-y-8 py-12 px-10 bg-[#F8F9FA]"> {/* Added background color */}
+      <section className="text-center space-y-8 py-12 px-10 bg-[#F8F9FA] mt-12">
         <h1 className="text-4xl md:text-5xl font-bold text-[#1A365D] animate-float">
           Welcome to Poornima Placements
         </h1>
@@ -51,27 +51,28 @@ const Home = () => {
       </section>
 
       {/* Stats Section */}
-      <section className="bg-[#F8F9FA] py-12 px-10">
+      <section className="bg-[#F8F9FA] py-12 px-10 mt-22">
   <div className="container mx-auto flex">
     
     {/* Stat 1 */}
-    <div className="w-1/3 flex items-center pl-10 bg-white rounded-lg px-40 p border-2 py-5 m-8">
+    <div className="w-1/3 flex items-center pl-10 bg-white rounded-lg px-70 p border-2 py-5 m-8 hover:shadow-lg transition duration-300">
       <LuBriefcase size={30} className="mr-4 bg-gray-200 p-1 rounded-lg text-[#1A365D]" />
-      <div>
+      <div className='hover:shadow-lg transition duration-300'>
         <span className="text-2xl font-bold text-[#1A365D] block">500+</span>
         <p className="text-md text-gray-600">Active Jobs</p>
       </div>
     </div>
 
     {/* Stat 2 */}
-    <div className="w-1/3 flex items-center pl-10 bg-white rounded-lg px-40 p border-2 py-5 m-8">
+
+    <div className="w-1/3 flex items-center pl-10 bg-white rounded-lg px-70 p border-2 py-5 m-8 hover:shadow-lg transition duration-300">
     <TbUsers  size={30} className="mr-4 bg-gray-200 p-0.5 px-0.9 rounded-lg text-[#1A365D]" />
       <span className="text-2xl font-bold text-[#1A365D] block">1000+</span>
       <p className="text-md text-gray-600">Placed Students</p>
     </div>
 
     {/* Stat 3 */}
-    <div className="w-1/3 flex items-center pl-10 bg-white rounded-lg px-40 p border-2 py-5 m-8">
+    <div className="w-1/3 flex items-center pl-10 bg-white rounded-lg px-70 p border-2 py-5 m-8 hover:shadow-lg transition duration-300">
     <IoMdTrendingUp size={30} className="mr-4 bg-gray-200 p-1 rounded-lg text-[#1A365D]"  />
       <span className="text-2xl font-bold text-[#1A365D] block">92%</span>
       <p className="text-md text-gray-600">Placement Rate</p>
@@ -80,7 +81,7 @@ const Home = () => {
   </div>
 </section>
 
-<section className="bg-[#F8F9FA] py-0.5 px-10 text-center">
+<section className="bg-[#F8F9FA] py-0.5 mb-10 px-10 text-center">
    <h2 className="text-3xl font-bold text-[#1A365D] mb-8">Meet Our Placement Team</h2>
    <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
      {teamMembers.map((member, index) => (
@@ -102,6 +103,22 @@ const Home = () => {
      ))}
    </div>
  </section>
+
+    <section className="space-y-6 bg-[#F8F9FA] ">
+        <h2 className="text-2xl font-bold text-center text-primary">
+          Featured Companies
+        </h2>
+        <div className="grid grid-cols-2 md:grid-cols-4 p-8 gap-8">
+          {[1, 2, 3, 4].map((i) => (
+            <div
+              key={i}
+              className="aspect-video bg-white border-2 hover:border-gray-900 rounded-lg shadow-sm p-4 flex items-center justify-center"
+            >
+              <div className="text-gray-400 ">Company Logo</div>
+            </div>
+          ))}
+        </div>
+      </section>
 
  
 
